c9a16968e2b4cec25f0b9c1255d5a74e535fa75c
Finlay O'Loughlin <finlayoloughlin@gmail.com>
2024-05-14 16:59:52 +0800

    submission separates the placeholders from content.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash
 from app import flaskApp, db
-from app.models import User, Madlib
+from app.models import User, Madlib, Placeholder
+import re
 
 @flaskApp.route("/")
 @flaskApp.route("/home")
@@ -31,16 +32,34 @@ def Register():
 
         return redirect(location=url_for("home"))
     
+def get_next_id():
+    last_id = db.session.query(Madlib.id).order_by(Madlib.id.desc()).first()
+    if last_id:
+        return last_id[0] + 1
+    else:
+        return 1
+    
 @flaskApp.route("/submit", methods=['POST'])
 def Submit():
     if request.method =='POST':
         content = request.form.get('content')
+
+        placeholders = re.findall(r'\[(.*?)\]', content)
+
+        print(placeholders)
+
+        next_id = get_next_id()
+
+        madlib = Madlib(id=next_id, content=content)
+
+        for placeholder_value in placeholders:
+            placeholder = Placeholder(value=placeholder_value, madlib=madlib)
+            madlib.placeholders.append(placeholder)
     
-        game = Madlib(content=content)
-        db.session.add(game)
+        db.session.add(madlib)
         db.session.commit()
 
-        print(Madlib.query.all())
+        print(Madlib.query.order_by(Madlib.id.desc()).first())
         flash("Submitted madlib successfully", "success")
 
         return redirect(location=url_for("home"))
