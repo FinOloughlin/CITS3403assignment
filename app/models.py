1e3d7cd1b9d3e39600a38520b506e44a9ab08de7
Finlay O'Loughlin <finlayoloughlin@gmail.com>
2024-05-20 13:18:30 +0800

    hopefully works

--- app/models.py
+++ app/models.py
@@ -8,23 +8,8 @@ class User(db.Model):
     def __repr__(self) -> str:
         return f'<Student {self.email} {self.password}>'
     
-class Madlib(db.Model):
-    __tablename__ = 'Madlib'
+class MadLib(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(1000), nullable=False)
-
-    placeholders = db.relationship('Placeholder', back_populates='madlib')
-
-    def __repr__(self) -> str:
-        return f'<Madlib id = "{self.id}", content = "{self.content}", placeholders = "{self.placeholders}">'
-    
-class Placeholder(db.Model):
-    __tablename__ = 'Placeholder'
-    id = db.Column(db.Integer, primary_key=True)
-    madlib_id = db.Column(db.Integer, db.ForeignKey('Madlib.id'))
-    value = db.Column(db.String(255))
-
-    madlib = db.relationship('Madlib', back_populates='placeholders')
-
-    def __repr__(self) -> str:
-        return f'<{self.value}>'
+    story = db.Column(db.String(1000), nullable=False)
+    questions = db.Column(db.String(1000), nullable=False)
+    placeholders = db.Column(db.String(1000), nullable=False)
